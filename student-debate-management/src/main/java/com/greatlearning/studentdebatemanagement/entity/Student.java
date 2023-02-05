b5d251446ef55754f475acf9405c2efb5f840213
Joel John <110528386+joeljohn9699@users.noreply.github.com>
2023-02-05 20:18:04 +0530

    Add files via upload

---
+++ student-debate-management/src/main/java/com/greatlearning/studentdebatemanagement/entity/Student.java
@@ -0,0 +1,26 @@
+package com.greatlearning.studentdebatemanagement.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String firstName;
+	private String lastName;
+	private String course;
+	private String country;
+
+}
